957341262446783408afd811bd997f309923946c
David Reeves <djreeves2005@gmail.com>
2020-07-26 14:43:40 -0400

    Test for android

--- LightMasterMVVM/LightMasterMVVM/ViewModels/MainWindowViewModel.cs
+++ LightMasterMVVM/LightMasterMVVM/ViewModels/MainWindowViewModel.cs
@@ -25,6 +25,7 @@ using System.Runtime.CompilerServices;
 using iMobileDevice.iDevice;
 using iMobileDevice;
 using iMobileDevice.Lockdown;
+using System.IO.Ports;
 
 namespace LightMasterMVVM.ViewModels
 {
@@ -1801,6 +1802,16 @@ namespace LightMasterMVVM.ViewModels
                     Console.WriteLine(deviceName);
                 }
             }
+
+            string[] ports = SerialPort.GetPortNames();
+            Console.WriteLine("The following serial ports were found:");
+            // Display each port name to the console.             
+            foreach (string port in ports)
+            {
+                Console.WriteLine(port);
+            }
+            Console.ReadLine();
+        
         }
         private void ReceiveDataFromDevice(iDeviceConnectionHandle connection, IiDeviceApi deviceApi)
         {
