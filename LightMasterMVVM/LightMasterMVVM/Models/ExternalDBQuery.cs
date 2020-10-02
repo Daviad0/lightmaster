742a4c8c835f61586485bc05fd6dbe3c92c4e609
David Reeves <djreeves2005@gmail.com>
2020-10-01 20:39:51 -0400

    DBQuery models and model compression

---
+++ LightMasterMVVM/LightMasterMVVM/Models/ExternalDBQuery.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace LightMasterMVVM.Models
+{
+    public class ExternalDBQuery
+    {
+        [JsonProperty("ODR")]
+        public List<string> OrderByProperties { get; set; }
+        [JsonProperty("RTN")]
+        public List<string> ReturnProperties { get; set; }
+        [JsonProperty("MAX")]
+        public int MaxQuerySize { get; set; }
+        [JsonProperty("INC")]
+        public List<string> IncludedTeams { get; set; }
+
+    }
+    public class ExternalDBQueryResult
+    {
+        // HOI IM TEMMIE
+        // HUMANS... SUCH A- CUTE~~
+        [JsonProperty("TEM")]
+        public int TeamNumber { get; set; }
+        [JsonProperty("RLT")]
+        public List<int> CorrespondingResults { get; set; }
+    }
+}
--- LightMasterMVVM/LightMasterMVVM/Models/IO_TeamMatch.cs
+++ LightMasterMVVM/LightMasterMVVM/Models/IO_TeamMatch.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,36 +7,67 @@ namespace LightMasterMVVM.Models
 {
     public class IO_TeamMatch
     {
+        [JsonProperty("ID")]
         public int MatchID { get; set; }
+        [JsonProperty("T#")]
         public int TeamNumber { get; set; }
+        [JsonProperty("COL")]
         public string TabletId { get; set; }
+        [JsonProperty("Q?")]
         public bool IsQualifying { get; set; }
+        [JsonProperty("NAME")]
         public string TeamName { get; set; }
+        [JsonProperty("E#")]
         public string EventCode { get; set; }
+        [JsonProperty("M#")]
         public int MatchNumber { get; set; }
+        [JsonProperty("POS")]
         public string RobotPosition { get; set; }
+        [JsonProperty("SCT")]
         public string ScoutName { get; set; }
+        [JsonProperty("ALN")]
         public bool A_InitiationLine { get; set; }
+        [JsonProperty("PCM")]
         public int[] PowerCellMissed { get; set; }
+        [JsonProperty("PCI")]
         public int[] PowerCellLower { get; set; }
+        [JsonProperty("PCL")]
         public int[] PowerCellOuter { get; set; }
+        [JsonProperty("PCO")]
         public int[] PowerCellInner { get; set; }
+        [JsonProperty("CYC#")]
         public int NumCycles { get; set; }
+        [JsonProperty("CPR")]
         public bool T_ControlPanelRotation { get; set; }
+        [JsonProperty("CPP")]
         public bool T_ControlPanelPosition { get; set; }
+        [JsonProperty("PRK")]
         public bool E_Park { get; set; }
+        [JsonProperty("CMBA")]
         public bool E_ClimbAttempt { get; set; }
+        [JsonProperty("CMBS")]
         public bool E_ClimbSuccess { get; set; }
+        [JsonProperty("BAL")]
         public bool E_Balanced { get; set; }
+        [JsonProperty("DSEC")]
         public int DisabledSeconds { get; set; }
+        [JsonProperty("SBM")]
         public bool ClientSubmitted { get; set; }
+        [JsonProperty("LSBM")]
         public DateTime ClientLastSubmitted { get; set; }
+        [JsonProperty("LOGS")]
         public string[] TapLogs { get; set; }
+        [JsonProperty("WITH")]
         public int[] AlliancePartners { get; set; }
+        [JsonProperty("CYCS")]
         public int CycleTime { get; set; }
+        [JsonProperty("DEFF")]
         public bool DefenseFor { get; set; }
+        [JsonProperty("DEFA")]
         public bool DefenseAgainst { get; set; }
+        [JsonProperty("COSH")]
         public int[] ShotCoordinates { get; set; }
+        [JsonProperty("COLD")]
         public int[] LoadCoordinates { get; set; }
     }
 }
